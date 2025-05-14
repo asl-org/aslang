0dc9d1840870017842697c15d66ec74b664e66f8
Shubham Singh <ss3681755@gmail.com>
2025-05-14 13:55:00 +0530

    [Project Euler] - Refactor problem 2 solution to both recursive and iterative implementations.

--- project-euler/problem-002/solution.c
+++ project-euler/problem-002/solution.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int solve_recur(int a, int b, int c, int d)
 {
-  int a = 1;
-  int b = 2;
-  int c = 4000000;
-  int d = 0;
+  if (a > c)
+  {
+    return d;
+  }
+  int e = a & 1 ? d : d + a;
+  return solve_recur(b, a + b, c, e);
+}
+
+int solve_iter(int a, int b, int c, int d)
+{
   while (a <= c)
   {
     if ((a & 1) == 0)
@@ -16,6 +22,16 @@ int main(int argc, char const *argv[])
     a = b;
     b = e;
   }
-  printf("%d\n", d);
+  return d;
+}
+
+int main(int argc, char const *argv[])
+{
+  int a = 1;
+  int b = 2;
+  int c = 4000000;
+  int d = 0;
+  printf("%d\n", solve_iter(a, b, c, d));
+  // printf("%d\n", solve_recur(a, b, c, d));
   return 0;
 }
