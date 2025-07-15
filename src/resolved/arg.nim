8d3638a3b970fcfd09123293e541024c6f5aed08
Shubham Singh <ss3681755@gmail.com>
2025-07-15 08:49:02 +0530

    [Refactor] - Change resolve argument function signature.

--- src/resolved/arg.nim
+++ src/resolved/arg.nim
@@ -4,5 +4,5 @@ type ResolvedArgument* = ref object of RootObj
   kind: Token
   value*: Token
 
-proc new_resolved_expression*(kind: Token, value: Token): ResolvedArgument =
+proc new_resolved_argument*(kind: Token, value: Token): ResolvedArgument =
   ResolvedArgument(kind: kind, value: value)
