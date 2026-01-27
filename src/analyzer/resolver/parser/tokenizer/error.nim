61e748411543e867b0c86454ca5b6ac2346b92f5
Shubham Singh <ss3681755@gmail.com>
2026-01-27 11:01:22 +0530

    [Tokenizer] - Refactor Error.

--- src/analyzer/resolver/parser/tokenizer/error.nim
+++ src/analyzer/resolver/parser/tokenizer/error.nim
@@ -1,27 +1,27 @@
 import strformat
 
-type TokenizerError* = ref object of RootObj
+type Error* = ref object of RootObj
   index: int
   message: string
 
-proc `<`*(self: TokenizerError, other: TokenizerError): bool = self.index < other.index
-proc `>`*(self: TokenizerError, other: TokenizerError): bool = self.index > other.index
-proc `==`*(self: TokenizerError, other: TokenizerError): bool = self.index == other.index
-proc `$`*(err: TokenizerError): string = err.message
+proc `<`*(self: Error, other: Error): bool = self.index < other.index
+proc `>`*(self: Error, other: Error): bool = self.index > other.index
+proc `==`*(self: Error, other: Error): bool = self.index == other.index
+proc `$`*(err: Error): string = err.message
 
-proc new_tokenizer_error(index: int, message: string): TokenizerError =
-  TokenizerError(index: index, message: message)
+proc new_tokenizer_error(index: int, message: string): Error =
+  Error(index: index, message: message)
 
-proc err_tokenizer_reached_eof*(index: int): TokenizerError =
+proc err_tokenizer_reached_eof*(index: int): Error =
   new_tokenizer_error(index, "tokenizer reached end of file")
 
 proc err_tokenizer_expectation_mismatch*(index: int, expected: string,
-    found: string): TokenizerError =
+    found: string): Error =
   new_tokenizer_error(index, fmt"expected `{expected}` found `{found}`")
 
 proc err_tokenizer_unexpected_escape_sequence*(index: int,
-    escape_seq: string): TokenizerError =
+    escape_seq: string): Error =
   new_tokenizer_error(index, fmt"unexpected string escape sequence encountered: `{escape_seq}`")
 
-proc err_tokenizer_new_line_in_string*(index: int): TokenizerError =
+proc err_tokenizer_new_line_in_string*(index: int): Error =
   new_tokenizer_error(index, fmt"encountered new line character while parsing string literal")
